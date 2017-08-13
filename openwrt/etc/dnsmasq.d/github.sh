a307b2c74acdeebc62eae2889f24f903091f5658
owoin <admin@owoin.com>
2017-08-13 07:48:09 +0000

    Update github.sh

--- openwrt/etc/dnsmasq.d/github.sh
+++ openwrt/etc/dnsmasq.d/github.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-wget -P /etc -O /etc/pdnsd.conf --no-check-certificate https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/pdnsd.conf \
-&& wget -P /etc -O /etc/resolv.dnsmasq.conf --no-check-certificate https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/resolv.dnsmasq.conf \
-&& wget -P /etc -O /etc/dnsmasq.conf --no-check-certificate https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/dnsmasq.conf \
-&& wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/gfw.conf --no-check-certificate https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/dnsmasq.d/conf/gfw.conf \
+wget -P /etc -O /etc/pdnsd.conf --no-check-certificate https://gitlab.com/owoin/dnsmasq/raw/master/openwrt/etc/pdnsd.conf \
+&& wget -P /etc -O /etc/resolv.dnsmasq.conf --no-check-certificate https://gitlab.com/owoin/dnsmasq/raw/master/openwrt/etc/resolv.dnsmasq.conf \
+&& wget -P /etc -O /etc/dnsmasq.conf --no-check-certificate https://gitlab.com/owoin/dnsmasq/raw/master/openwrt/etc/dnsmasq.conf \
+&& wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/gfw.conf --no-check-certificate https://gitlab.com/owoin/dnsmasq/raw/master/openwrt/etc/dnsmasq.d/conf/gfw.conf \
 && /etc/init.d/dnsmasq stop & /etc/init.d/dnsmasq start \
 && /etc/init.d/pdnsd restart
