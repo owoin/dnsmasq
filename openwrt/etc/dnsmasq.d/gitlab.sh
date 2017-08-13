1e1776bcc1d7cd51f7c611b57b6ba2525a9d5c24
owoin <admin@owoin.com>
2017-08-13 08:12:55 +0000

    Add new file

---
+++ openwrt/etc/dnsmasq.d/gitlab.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+wget -P /etc -O /etc/pdnsd.conf --no-check-certificate https://gitlab.com/owoin/dnsmasq/raw/master/openwrt/etc/pdnsd.conf \
+&& wget -P /etc -O /etc/resolv.dnsmasq.conf --no-check-certificate https://gitlab.com/owoin/dnsmasq/raw/master/openwrt/etc/resolv.dnsmasq.conf \
+&& wget -P /etc -O /etc/dnsmasq.conf --no-check-certificate https://gitlab.com/owoin/dnsmasq/raw/master/openwrt/etc/dnsmasq.conf \
+&& wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/gfw.conf --no-check-certificate https://gitlab.com/owoin/dnsmasq/raw/master/openwrt/etc/dnsmasq.d/conf/gfw.conf \
+&& wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/ad.conf --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf \
+&& wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/isp.conf --no-check-certificate hhttps://github.com/vokins/yhosts/raw/master/dnsmasq/ip.conf \
+
+&& /etc/init.d/dnsmasq stop & /etc/init.d/dnsmasq start \
+&& /etc/init.d/pdnsd restart
