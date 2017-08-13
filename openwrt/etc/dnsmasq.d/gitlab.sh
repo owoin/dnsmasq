69bb94846a3ee84724320e458f1c12f3785fd34d
owoin <admin@owoin.com>
2017-08-13 08:25:02 +0000

    Update gitlab.sh

--- openwrt/etc/dnsmasq.d/gitlab.sh
+++ openwrt/etc/dnsmasq.d/gitlab.sh
@@ -5,6 +5,6 @@ wget -P /etc -O /etc/pdnsd.conf --no-check-certificate https://gitlab.com/owoin/
 && wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/gfw.conf --no-check-certificate https://gitlab.com/owoin/dnsmasq/raw/master/openwrt/etc/dnsmasq.d/conf/gfw.conf \
 && wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/ad.conf --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf \
 && wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/isp.conf --no-check-certificate hhttps://github.com/vokins/yhosts/raw/master/dnsmasq/ip.conf \
-
+\
 && /etc/init.d/dnsmasq stop & /etc/init.d/dnsmasq start \
 && /etc/init.d/pdnsd restart
