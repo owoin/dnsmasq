7d9aeee1755304c191a561c1f4bee6df45d7e277
owoin <owoin@users.noreply.github.com>
2017-08-13 18:55:29 +0800

    Update github.sh

--- openwrt/etc/dnsmasq.d/github.sh
+++ openwrt/etc/dnsmasq.d/github.sh
@@ -4,6 +4,8 @@ wget -P /etc -O /etc/pdnsd.conf --no-check-certificate https://raw.githubusercon
 && wget -P /etc -O /etc/dnsmasq.conf --no-check-certificate https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/dnsmasq.conf \
 && wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/gfw.conf --no-check-certificate https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/dnsmasq.d/conf/gfw.conf \
 && wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/ad.conf --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf \
-&& wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/isp.conf --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/ip.conf \
+&& wget -P /etc/dnsmasq.d/ -O /etc/dnsmasq.d/isp.conf --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/ip.conf \
+&& cat /etc/dnsmasq.d/isp.conf >> /etc/dnsmasq.conf \
+&& rm /etc/dnsmasq.d/isp.conf \
 && /etc/init.d/dnsmasq stop & /etc/init.d/dnsmasq start \
 && /etc/init.d/pdnsd restart
