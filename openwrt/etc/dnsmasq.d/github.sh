3595d814c8a6b887edb84bd0eaabd11673c6d140
owoin <owoin@users.noreply.github.com>
2017-08-13 19:07:27 +0800

    Update github.sh

--- openwrt/etc/dnsmasq.d/github.sh
+++ openwrt/etc/dnsmasq.d/github.sh
@@ -3,7 +3,7 @@ wget -P /etc -O /etc/pdnsd.conf --no-check-certificate https://raw.githubusercon
 && wget -P /etc -O /etc/resolv.dnsmasq.conf --no-check-certificate https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/resolv.dnsmasq.conf \
 && wget -P /etc -O /etc/dnsmasq.conf --no-check-certificate https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/dnsmasq.conf \
 && wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/gfw.conf --no-check-certificate https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/dnsmasq.d/conf/gfw.conf \
-&& wget -P /etc/dnsmasq.d/ -O /etc/dnsmasq.d/isp.conf --no-check-certificate https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/dnsmasq.d/isp.conf \
+&& wget -P /etc/dnsmasq.d -O /etc/dnsmasq.d/isp.conf --no-check-certificate https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/dnsmasq.d/isp.conf \
 && wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/ad.conf --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf \
 && cat /etc/dnsmasq.d/isp.conf >> /etc/dnsmasq.conf \
 && rm /etc/dnsmasq.d/isp.conf \
