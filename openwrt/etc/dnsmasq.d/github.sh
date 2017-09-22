4ae63202f1b9b78cd581e0e7c5301443b352bfe4
debian <debian@dell.lan>
2017-09-22 12:38:24 +0800

    update

--- openwrt/etc/dnsmasq.d/github.sh
+++ openwrt/etc/dnsmasq.d/github.sh
@@ -7,5 +7,5 @@ wget -P /etc -O /etc/pdnsd.conf --no-check-certificate https://raw.githubusercon
 && wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/ad.conf --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf \
 && cat /etc/dnsmasq.d/conf/isp.conf >> /etc/dnsmasq.conf \
 && rm /etc/dnsmasq.d/conf/isp.conf \
-&& killall dnsmasq && /etc/init.d/dnsmasq start \
-&& /etc/init.d/pdnsd restart
+&& killall dnsmasq && /etc/init.d/dnsmasq start && /etc/init.d/dnsmasq restart \
+&& killall pdnsd && /etc/init.d/pdnsd start && /etc/init.d/pdnsd restart
