793c37471e919b1a4e0fff2a53a8552a34ef315c
owoin <admin@owoin.com>
2017-10-03 11:35:52 +0800

    update

--- openwrt/etc/dnsmasq.d/github.sh
+++ openwrt/etc/dnsmasq.d/github.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 wget -P /etc -O /etc/pdnsd.conf --no-check-certificate  https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/pdnsd.conf \
-&& wget -P /etc -O /etc/resolv.dnsmasq.conf --no-check-certificate  https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrtetc/resolv.dnsmasq.conf \
+&& wget -P /etc -O /etc/resolv.dnsmasq.conf --no-check-certificate  https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/resolv.dnsmasq.conf \
 && wget -P /etc -O /etc/dnsmasq.conf --no-check-certificate  https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/dnsmasq.conf \
 && wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/gfw.conf --no-check-certificate  https://raw.githubusercontent.com/owoin/dnsmasq/master/openwrt/etc/dnsmasq.d/conf/gfw.conf \
 && wget -P /etc/dnsmasq.d/conf -O /etc/dnsmasq.d/conf/ad.conf --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf \
